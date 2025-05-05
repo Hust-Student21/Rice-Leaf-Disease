8e7ce1dcd25b22f1ad8c494a27faf172eb6175bb
Hust-Student21 <son.cvh.eng2110@gmail.com>
2025-05-05 17:58:53 +0700

    relaunch

--- compare.py
+++ compare.py
@@ -3,16 +3,16 @@ import os
 
 # Danh sách các file model muốn kiểm tra
 model_files = {
-    #"YOLOv8 Gốc (.pt)": r'yolov8n.pt',
-    #"YOLOv8 Trained (.pt)": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\runs\train\100e_20_3\weights\100e203.pt',
-    #"YOLOv8 ONNX (.onnx)": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\runs\train\100e_20_3\weights\100e203.onnx',
-    #"YOLOv8 Pruned (.onnx)": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\100e203_pruned.pt',
-    #"YOLOv8 Quantized (.onnx)":r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\100e203_quantitized.onnx',
-    #"YOLOv8 Float16 (.onnx)":r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\100e203_fp16.onnx',
-    #"YOLOv8 ghost":r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\runs\train\ghost_100_293\weights\ghost100.pt',
+    "YOLOv8 Gốc (.pt)": r'yolov8n.pt',
+    "YOLOv8 Trained (.pt)": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\runs\train\100e_20_3\weights\100e203.pt',
+    "YOLOv8 ONNX (.onnx)": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\runs\train\100e_20_3\weights\100e203.onnx',
+    "YOLOv8 Pruned (.onnx)": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\100e203_pruned.pt',
+    "YOLOv8 Quantized (.onnx)":r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\100e203_quantitized.onnx',
+    "YOLOv8 Float16 (.onnx)":r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\100e203_fp16.onnx',
+    "YOLOv8 ghost":r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\runs\train\ghost_100_293\weights\ghost100.pt',
     "Ghost onnx": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\ghost_int8_dyna.onnx',
-    #"ghost_fp16": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\ghost_fp16.onnx',
-    #"gema": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\runs\train\ema_org\weights\gema.pt',
+    "ghost_fp16": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\ghost_fp16.onnx',
+    "gema": r'D:\STUDY_HARD\Uni\2024.2\THESIS\Rice-Plant-Disease-Detection\runs\train\ema_org\weights\gema.pt',
     # Thêm các file khác nếu cần
 }
 
